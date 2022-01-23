ca8330f3a4a0042d004ce3ed3d3b6236ab43277c
Bent <bent.stevens@student.howest.be>
2022-01-23 21:20:34 +0100

    added content in basic html

--- src/view/pages/index.php
+++ src/view/pages/index.php
@@ -1,1 +1,162 @@
-<h1>HElLOOO WORLD</h1>
+<section>
+  <div>
+    <h1>The Story of Caslon</h1>
+    <p>Discover the history of one of the most iconic typefaces of all time. Told by William Caslon himself. The founder and creator of the Caslon typeface, a serif font with a rich history.</p>
+  </div>
+  <p>presented by Slanted Magazine</p>
+</section>
+
+<section>
+  <div>
+    <h1>Hi, i'm William Caslon!</h1>
+    <p>I am the creator of the legendary typeface Caslon. But I will tell you more about that later. Like every story my story starts at the beginning…</p>
+  </div>
+
+  <div>
+    <img src="" alt="">
+  </div>
+</section>
+
+<section>
+  <p>1692</p>
+
+  <div>
+    <h2>Chapter 1</h2>
+    <h1>The simple life</h1>
+    <p>It was a cold morning in the year 1692. I was born in a small village named Cradley, Worcestershire. I had a hard working family and from early on I was trained to be an engraver. Engraving is one of the oldest and most important techniques when producing images on paper in artistic printmaking. It was a simple life. Around the age of seventeen, I became an apprentice to a local engraver of gunlocks and barrels. That’s where I started to learn all about the ins and out of metalwork.</p>
+  </div>
+</section>
+
+<section>
+  <h1>My call</h1>
+  <h2>to adventure</h2>
+
+  <div>
+    <p>In 1716 I started a business in London. I was an official engraver of gun locks and barrels, and made tools for bookbinders. Metalwork is a form of art in my opinion, it requires precision and patience.</p>
+    <p>For seven years I did numerous assignments for many different people. My metal craftsmanship was noticed by the printer John Watts. He gave me the task of cutting type punches for various presses in London. I challenged myself and took the job.</p>
+    <p>It's from that moment that I became more and more interested in the art of letter engraving.</p>
+  </div>
+
+  <div>
+    <img src="" alt="">
+  </div>
+</section>
+
+<section>
+  <blockquote>I fell in love with making type punches and was very good at it, if I say so myself. </blockquote>
+  <img src="" alt="">
+  <img src="" alt="">
+</section>
+
+<section>
+  <h2>Chapter 2</h2>
+  <h1>My first love</h1>
+  <p>I fell in love with making type punches and was very good at it, if I say so myself. In the years that followed, I took on countless of other jobs for companies that had to do with letter engraving. In 1720 William Bowyer asked me to design an English Arabic for Christian publications to be distributed in the Middle East. The typeface is now used in psalter and the New Testament. William saw the passion in me and we became good friends. Through jobs like this, the word spread about my type engraving.</p>
+</section>
+
+<section>
+  <h1>With a little help from my friends</h1>
+  <p>Although my success with the typefaces in England were almost instantaneous, I did not do this on my own. Banks gave me loans and sufficient money to go on. Having contact with printers, I was induced to start up a type foundry, largely through the encouragement of my dear friend William Bowyer. And off course, then there was my lovely wife Elizabeth who assisted me in getting the foundry reputation going. I met Elizabeth in the first years that I worked in London. </p>
+</section>
+
+<section>
+  <h2>Chapter 3</h2>
+  <h1>Successful, but incomplete</h1>
+  <p>Sure there where challenges, moments of doubt and setbacks. But from 1720 to 1780 few books were printed in England that did not use type from my foundry. Yes, I made a lot of typefaces in my years as a type founder. Still I felt a burning desire to keep moving. Why did I leave my simple life and home town? Well, I wanted to have a lasting impact on the world. And now I was ready.</p>
+</section>
+
+<section>
+  <blockquote>When in doubt, use Caslon</blockquote>
+</section>
+
+<section>
+  <div>
+    <p></p>
+    <p></p>
+  </div>
+
+  <div>
+    <form action="">
+      <label for="input">Make your own specimen sheet!</label><br>
+      <input type="text" id="input" name="input">
+
+      <button>Reset</button>
+      <input type="submit" value="Submit">
+    </form>
+  </div>
+
+  <article>
+    <div>
+      <h2>Caslon regular</h2>
+      <p>abcdefghjknoprstwxy</p>
+      <p>ABDEGHJMQRSW</p>
+    </div>
+
+    <div>
+      <h2>caslon italic</h2>
+      <p>abcdefghjknoprstwxy</p>
+      <p>ABDEGHJMQRSW</p>
+    </div>
+
+    <div>
+      <h2>Caslon bold</h2>
+      <p>abcdefghjknoprstwxy</p>
+      <p>ABDEGHJMQRSW</p>
+    </div>
+  </article>
+
+</section>
+
+<section>
+  <h2>Chapter 4</h2>
+  <h1>Changing the world</h1>
+
+  <div>
+    <p>Caslon is a versatile typeface that can be found in a wide variety of places. Places where history has been written. My typeface was considered the font of choice for royalty and politics.</p>
+    <p>I surpassed all of my competitors in no time and became the exclusive type foundry for the King’s printers. Many great works of literature and documents of historical importance used my Caslon typeface.</p>
+    <p>It was used in the first printed version of the United States Declaration of Independence. And George Bernard Shaw asked specifically that all his plays were set in the Caslon typeface. I never met George, but he seems like a nice guy.</p>
+  </div>
+
+  <div>
+    <img src="" alt="">
+  </div>
+</section>
+
+<section>
+  <h2>Chapter 5</h2>
+  <h1>What makes my Caslon type so special, u might ask?</h1>
+  <p>The Caslon typeface is extremely legible, yet graceful and classic. That is why a lot of printers like to use it for printed communication pieces. I designed the typeface with the Old-style typeface characteristics in mind. I like the look of the Old-style typefaces because of their greater refinement.</p>
+</section>
+
+<section>
+  <img src="" alt="">
+  <div>
+    <p>wedged shape</p>
+    <p>horizontal crossbar</p>
+    <p>upright position</p>
+    <p>thin and thick strokes</p>
+  </div>
+
+  <article>
+    <div>
+      <p>There is a greater contrast between the thin and thick strokes, and that's why they are generally speaking sharper in appearance. </p>
+      <p>Another major characteristic is the upright stress of the letterforms. Old-style typefaces have a more upright position.</p>
+    </div>
+
+    <div>
+      <p>Perhaps the most notable characteristic is that the ascenders have a wedged shape. </p>
+      <p>The horizontal crossbars, that you can see by the “e” for example, are also typical for Old-style typefaces. This is something that is not the case with the Venetian typefaces. That is why i don't like them so much. I hate them frankly.</p>
+    </div>
+  </article>
+</section>
+
+
+<section>
+  <h1>This was the story of Caslon</h1>
+  <h2>Liked this experience?</h2>
+  <p>This is a series where we bring some legendary fonts into focus. Every week we release a new immersive experience that brings you the rich typographical history and the fascinating designers of a certain typeface. Brought to you by Slanted.</p>
+  <a href="">more info about the selected typfaces </a>
+</section>
+
+
+
