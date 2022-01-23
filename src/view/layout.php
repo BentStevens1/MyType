86ec9fe3914851b5ddc743bec9b6fe927312058f
Bent <bent.stevens@student.howest.be>
2022-01-23 21:45:49 +0100

    deleted template message

--- src/view/layout.php
+++ src/view/layout.php
@@ -9,7 +9,7 @@
 </head>
 <body>
   <div class="container">
-      <header><h1 class="pagetitle">Webpack - PHP - Template</h1></header>
+      <header></header>
       <?php echo $content;?>
   </div>
   <?php echo $js; ?>
