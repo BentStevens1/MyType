f5364aa856a9385496e9ac6f3cbaef976f9acd74
Bent <bent.stevens@student.howest.be>
2022-01-24 16:26:28 +0100

    added lots of classes

--- src/view/pages/index.php
+++ src/view/pages/index.php
@@ -1,120 +1,123 @@
-<section>
+<section class="header">
   <div>
-    <h1>The Story of Caslon</h1>
-    <p>Discover the history of one of the most iconic typefaces of all time. Told by William Caslon himself. The founder and creator of the Caslon typeface, a serif font with a rich history.</p>
+    <h1 class= "header__title">The Story of Caslon</h1>
+    <p class= "body__text header__text">Discover the history of one of the most iconic typefaces of all time. Told by William Caslon himself. The founder and creator of the Caslon typeface, a serif font with a rich history.</p>
   </div>
-  <p>presented by Slanted Magazine</p>
+  <p class= "header__slanted">presented by Slanted Magazine</p>
+</section>
+
+<section class= "introduction">
+  <h1 class= "intro__title">Hi, i'm William Caslon!</h1>
+  <div>
+    <img src="" alt="">
+    <p class= "body__text intro__text"> I am the creator of the legendary typeface Caslon. But I will tell you more about that later. Like every story my story starts at the beginning…</p>
+  </div>
+</section>
+
+<section class= "part__1">
+  <p class= "title date">1692</p>
+
+  <div>
+    <h2 class= "chapter chapter__1">Chapter 1</h2>
+    <h1 class= "title title__life">The simple life</h1>
+    <p class= "body__text">It was a cold morning in the year 1692. I was born in a small village named Cradley, Worcestershire. I had a hard working family and from early on I was trained to be an engraver. Engraving is one of the oldest and most important techniques when producing images on paper in artistic printmaking. It was a simple life. Around the age of seventeen, I became an apprentice to a local engraver of gunlocks and barrels. That’s where I started to learn all about the ins and out of metalwork.</p>
+  </div>
 </section>
 
 <section>
-  <div>
-    <h1>Hi, i'm William Caslon!</h1>
-    <p>I am the creator of the legendary typeface Caslon. But I will tell you more about that later. Like every story my story starts at the beginning…</p>
-  </div>
+  <h1 class= " title title__call">My call</h1>
+  <h2 class= "title title__adventure">to adventure</h2>
 
   <div>
     <img src="" alt="">
   </div>
-</section>
-
-<section>
-  <p>1692</p>
 
   <div>
-    <h2>Chapter 1</h2>
-    <h1>The simple life</h1>
-    <p>It was a cold morning in the year 1692. I was born in a small village named Cradley, Worcestershire. I had a hard working family and from early on I was trained to be an engraver. Engraving is one of the oldest and most important techniques when producing images on paper in artistic printmaking. It was a simple life. Around the age of seventeen, I became an apprentice to a local engraver of gunlocks and barrels. That’s where I started to learn all about the ins and out of metalwork.</p>
+    <p class= "body__text body__text-padding">In 1716 I started a business in London. I was an official engraver of gun locks and barrels, and made tools for bookbinders. Metalwork is a form of art in my opinion, it requires precision and patience.</p>
+    <p class= "body__text body__text-padding">For seven years I did numerous assignments for many different people. My metal craftsmanship was noticed by the printer John Watts. He gave me the task of cutting type punches for various presses in London. I challenged myself and took the job.</p>
+    <p class= "body__text body__text-padding">It's from that moment that I became more and more interested in the art of letter engraving.</p>
   </div>
 </section>
 
-<section>
-  <h1>My call</h1>
-  <h2>to adventure</h2>
-
-  <div>
-    <p>In 1716 I started a business in London. I was an official engraver of gun locks and barrels, and made tools for bookbinders. Metalwork is a form of art in my opinion, it requires precision and patience.</p>
-    <p>For seven years I did numerous assignments for many different people. My metal craftsmanship was noticed by the printer John Watts. He gave me the task of cutting type punches for various presses in London. I challenged myself and took the job.</p>
-    <p>It's from that moment that I became more and more interested in the art of letter engraving.</p>
-  </div>
-
-  <div>
-    <img src="" alt="">
-  </div>
-</section>
-
-<section>
-  <blockquote>I fell in love with making type punches and was very good at it, if I say so myself. </blockquote>
+<section class= "quote">
+  <blockquote class= "title quote__text">I fell in love with making type punches and was very good at it, if I say so myself. </blockquote>
   <img src="" alt="">
   <img src="" alt="">
 </section>
 
-<section>
-  <h2>Chapter 2</h2>
-  <h1>My first love</h1>
-  <p>I fell in love with making type punches and was very good at it, if I say so myself. In the years that followed, I took on countless of other jobs for companies that had to do with letter engraving. In 1720 William Bowyer asked me to design an English Arabic for Christian publications to be distributed in the Middle East. The typeface is now used in psalter and the New Testament. William saw the passion in me and we became good friends. Through jobs like this, the word spread about my type engraving.</p>
+<section class= "part__2">
+  <h2 class= "chapter chapter__2">Chapter 2</h2>
+  <h1 class= "title title__love">My first love</h1>
+  <p class= "body__text-center body__text-padding">I fell in love with making type punches and was very good at it, if I say so myself. In the years that followed, I took on countless of other jobs for companies that had to do with letter engraving. In 1720 William Bowyer asked me to design an English Arabic for Christian publications to be distributed in the Middle East.</p>
+  <p class= "body__text-center body__text-padding">The typeface is now used in psalter and the New Testament. William saw the passion in me and we became good friends. Through jobs like this, the word spread about my type engraving.</p>
 </section>
 
-<section>
-  <h1>With a little help from my friends</h1>
-  <p>Although my success with the typefaces in England were almost instantaneous, I did not do this on my own. Banks gave me loans and sufficient money to go on. Having contact with printers, I was induced to start up a type foundry, largely through the encouragement of my dear friend William Bowyer. And off course, then there was my lovely wife Elizabeth who assisted me in getting the foundry reputation going. I met Elizabeth in the first years that I worked in London. </p>
+<section class= "part__2-1">
+  <h1 class= "title title__friends">With a little help from my friends</h1>
+  <p class= "body__text body__text-padding">Although my success with the typefaces in England were almost instantaneous, I did not do this on my own. Banks gave me loans and sufficient money to go on. Having contact with printers, I was induced to start up a type foundry, largely through the encouragement of my dear friend William Bowyer.</p>
+  <p class= "body__text body__text-padding">And off course, then there was my lovely wife Elizabeth who assisted me in getting the foundry reputation going. I met Elizabeth in the first years that I worked in London. </p>
 </section>
 
-<section>
-  <h2>Chapter 3</h2>
-  <h1>Successful, but incomplete</h1>
-  <p>Sure there where challenges, moments of doubt and setbacks. But from 1720 to 1780 few books were printed in England that did not use type from my foundry. Yes, I made a lot of typefaces in my years as a type founder. Still I felt a burning desire to keep moving. Why did I leave my simple life and home town? Well, I wanted to have a lasting impact on the world. And now I was ready.</p>
-</section>
+<div class= "white">
+  <section>
+    <h2 class= "chapter chapter__3">Chapter 3</h2>
+    <h1 class= "title title__success">Successful, but incomplete</h1>
+    <p class= "body__text">Sure there where challenges, moments of doubt and setbacks. But from 1720 to 1780 few books were printed in England that did not use type from my foundry. Yes, I made a lot of typefaces in my years as a type founder. Still I felt a burning desire to keep moving. Why did I leave my simple life and home town? Well, I wanted to have a lasting impact on the world. And now I was ready.</p>
+  </section>
 
-<section>
-  <blockquote>When in doubt, use Caslon</blockquote>
-</section>
+  <section class= "quote-slide">
+    <blockquote>When in doubt, use Caslon! When in doubt, use Caslon! When in doubt, use Caslon! When in doubt, use Caslon! When in doubt, use Caslon! When in doubt, use Caslon! When in doubt, use Caslon! When in doubt, use Caslon! When in doubt, use Caslon! When in doubt, use Caslon! When in doubt, use Caslon!</blockquote>
+  </section>
 
-<section>
-  <div>
-    <p></p>
-    <p></p>
-  </div>
-
-  <div>
-    <form action="">
-      <label for="input">Make your own specimen sheet!</label><br>
-      <input type="text" id="input" name="input">
-
-      <button>Reset</button>
-      <input type="submit" value="Submit">
-    </form>
-  </div>
-
-  <article>
+  <section>
     <div>
-      <h2>Caslon regular</h2>
-      <p>abcdefghjknoprstwxy</p>
-      <p>ABDEGHJMQRSW</p>
+      <p class= "body__text body__text-padding">In 1722 my friend William Bowyer saw my desire, acknowledged my talent and asked me to cut roman, italic, and Hebrew typefaces. A new roman typeface with lots of character. It was first used in 1726 and a few years later it came to be called Caslon. Named after me of course.</p>
+      <p class= "body__text body__text-padding">I issued my first official specimen sheet of the typeface in 1734 and exhibited the roman and italic types in 14 different sizes. The Caslon typeface eventually spread all over Europe and the American colonies. It became so popular that the expression “When in doubt, use Caslon” came about. I am really proud of that. </p>
     </div>
 
     <div>
-      <h2>caslon italic</h2>
-      <p>abcdefghjknoprstwxy</p>
-      <p>ABDEGHJMQRSW</p>
+      <form action="">
+        <label for="input" class= "title__specimen">Make your own specimen sheet!</label><br>
+        <input type="text" id="input" name="input" class= "input__field">
+        <div>
+          <button class= "button button-reset">Reset</button>
+          <input class= "button button-go" type="submit" value="GO!">
+        </div>
+
+      </form>
     </div>
 
-    <div>
-      <h2>Caslon bold</h2>
-      <p>abcdefghjknoprstwxy</p>
-      <p>ABDEGHJMQRSW</p>
-    </div>
-  </article>
+    <article>
+      <div>
+        <h2>Caslon regular</h2>
+        <p>abcdefghjknoprstwxy</p>
+        <p>ABDEGHJMQRSW</p>
+      </div>
 
-</section>
+      <div>
+        <h2>caslon italic</h2>
+        <p>abcdefghjknoprstwxy</p>
+        <p>ABDEGHJMQRSW</p>
+      </div>
+
+      <div>
+        <h2>Caslon bold</h2>
+        <p>abcdefghjknoprstwxy</p>
+        <p>ABDEGHJMQRSW</p>
+      </div>
+    </article>
+
+  </section>
+</div>
 
 <section>
-  <h2>Chapter 4</h2>
-  <h1>Changing the world</h1>
+  <h2 class= "chapter">Chapter 4</h2>
+  <h1 class= "title">Changing the world</h1>
 
   <div>
-    <p>Caslon is a versatile typeface that can be found in a wide variety of places. Places where history has been written. My typeface was considered the font of choice for royalty and politics.</p>
-    <p>I surpassed all of my competitors in no time and became the exclusive type foundry for the King’s printers. Many great works of literature and documents of historical importance used my Caslon typeface.</p>
-    <p>It was used in the first printed version of the United States Declaration of Independence. And George Bernard Shaw asked specifically that all his plays were set in the Caslon typeface. I never met George, but he seems like a nice guy.</p>
+    <p class= "body__text">Caslon is a versatile typeface that can be found in a wide variety of places. Places where history has been written. My typeface was considered the font of choice for royalty and politics.</p>
+    <p class= "body__text">I surpassed all of my competitors in no time and became the exclusive type foundry for the King’s printers. Many great works of literature and documents of historical importance used my Caslon typeface.</p>
+    <p class= "body__text">It was used in the first printed version of the United States Declaration of Independence. And George Bernard Shaw asked specifically that all his plays were set in the Caslon typeface. I never met George, but he seems like a nice guy.</p>
   </div>
 
   <div>
@@ -123,9 +126,9 @@
 </section>
 
 <section>
-  <h2>Chapter 5</h2>
-  <h1>What makes my Caslon type so special, u might ask?</h1>
-  <p>The Caslon typeface is extremely legible, yet graceful and classic. That is why a lot of printers like to use it for printed communication pieces. I designed the typeface with the Old-style typeface characteristics in mind. I like the look of the Old-style typefaces because of their greater refinement.</p>
+  <h2 class= "chapter">Chapter 5</h2>
+  <h class= "title"1>What makes my Caslon type so special, you might ask?</h1>
+  <p class= "body__text">The Caslon typeface is extremely legible, yet graceful and classic. That is why a lot of printers like to use it for printed communication pieces. I designed the typeface with the Old-style typeface characteristics in mind. I like the look of the Old-style typefaces because of their greater refinement.</p>
 </section>
 
 <section>
@@ -152,9 +155,9 @@
 
 
 <section>
-  <h1>This was the story of Caslon</h1>
+  <h1 class= "title">This was the story of Caslon</h1>
   <h2>Liked this experience?</h2>
-  <p>This is a series where we bring some legendary fonts into focus. Every week we release a new immersive experience that brings you the rich typographical history and the fascinating designers of a certain typeface. Brought to you by Slanted.</p>
+  <p class= "body__text">This is a series where we bring some legendary fonts into focus. Every week we release a new immersive experience that brings you the rich typographical history and the fascinating designers of a certain typeface. Brought to you by Slanted.</p>
   <a href="">more info about the selected typfaces </a>
 </section>
 
