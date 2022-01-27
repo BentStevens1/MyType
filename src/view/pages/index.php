31f8dacce8936f40196c9bdf8e6d66989a5c463a
Bent <bent.stevens@student.howest.be>
2022-01-27 16:13:37 +0100

    added classes for animation on html content #9

--- src/view/pages/index.php
+++ src/view/pages/index.php
@@ -8,11 +8,11 @@
 
 <section class= "introduction">
   <div class= "introduction__container">
-    <h1 class= "intro__title-mobile">Hi, i'm William Caslon!</h1>
-    <img class= "img__william" src="<?php echo $img_william ?>" alt="William-Caslon">
+    <h1 class= "intro__title-mobile gs_reveal">Hi, i'm William Caslon!</h1>
+    <img class= "img__william anim-2 gs_reveal" src="<?php echo $img_william ?>" alt="William-Caslon">
     <div class= "intro__text-container">
-      <h1 class= "intro__title-desktop">Hi, i'm William Caslon!</h1>
-      <p class= "body__text intro__text"> I am the creator of the legendary typeface Caslon. But I will tell you more about that later. Like every story my story starts at the beginning…</p>
+      <h1 class= "intro__title-desktop anim-1 gs_reveal">Hi, i'm William Caslon!</h1>
+      <p class= "body__text intro__text anim-1 gs_reveal"> I am the creator of the legendary typeface Caslon. But I will tell you more about that later. Like every story my story starts at the beginning…</p>
     </div>
   </div>
 </section>
@@ -22,38 +22,38 @@
 
   <div>
     <h2 class= "chapter chapter__1">Chapter 1</h2>
-    <h1 class= "title title__life">The simple life</h1>
-    <p class= "body__text">It was a cold morning in the year 1692. I was born in a small village named <span class= "highlight">Cradley, Worcestershire </span>. I had a hard working family and from early on I was trained to be an engraver. Engraving is one of the oldest and most important techniques when producing images on paper in artistic printmaking. It was a simple life. Around the age of seventeen, I became an apprentice to a local <span class= "highlight">engraver of gunlocks and barrels</span>. That's where I started to learn all about the ins and out of metalwork.</p>
+    <h1 class= "title title__life anim-1 gs_reveal">The simple life</h1>
+    <p class= "body__text anim-1 gs_reveal">It was a cold morning in the year 1692. I was born in a small village named <span class= "highlight">Cradley, Worcestershire </span>. I had a hard working family and from early on I was trained to be an engraver. Engraving is one of the oldest and most important techniques when producing images on paper in artistic printmaking. It was a simple life. Around the age of seventeen, I became an apprentice to a local <span class= "highlight">engraver of gunlocks and barrels</span>. That's where I started to learn all about the ins and out of metalwork.</p>
   </div>
 </section>
 
 <section class= "part__1-1">
-  <h1 class= " title title__call">My call</h1>
-  <h2 class= "title title__adventure">to adventure</h2>
+  <h1 class= " title title__call anim-1 gs_reveal">My call</h1>
+  <h2 class= "title title__adventure  anim-1 gs_reveal">to adventure</h2>
 
   <div class= "adventure__container">
     <div>
-      <img class= "img__guns" src="<?php echo $img_guns ?>" alt="">
+      <img class= "img__guns anim-1 gs_reveal" src="<?php echo $img_guns ?>" alt="">
     </div>
 
     <div class= "adventure__text">
-      <p class= "body__text body__text-padding">In <span class= "highlight">1716</span> I started a business in London. I was an official engraver of gun locks and barrels, and made tools for bookbinders. Metalwork is a form of art in my opinion, it requires precision and patience.</p>
-      <p class= "body__text body__text-padding">For seven years I did numerous assignments for many different people. My metal craftsmanship was noticed by <span class= "highlight">the printer John Watts</span>. He gave me the task of cutting type punches for various presses in London. I challenged myself and took the job.</p>
-      <p class= "body__text body__text-padding">It's from that moment that I became more and more interested in the <span class= "highlight">art of letter engraving</span>.</p>
+      <p class= "body__text body__text-padding anim-2 gs_reveal">In <span class= "highlight">1716</span> I started a business in London. I was an official engraver of gun locks and barrels, and made tools for bookbinders. Metalwork is a form of art in my opinion, it requires precision and patience.</p>
+      <p class= "body__text body__text-padding anim-2 gs_reveal">For seven years I did numerous assignments for many different people. My metal craftsmanship was noticed by <span class= "highlight">the printer John Watts</span>. He gave me the task of cutting type punches for various presses in London. I challenged myself and took the job.</p>
+      <p class= "body__text body__text-padding anim-2 gs_reveal">It's from that moment that I became more and more interested in the <span class= "highlight">art of letter engraving</span>.</p>
     </div>
 
   </div>
 
 </section>
 
-<section class= "quote">
+<section class= "quote anim-3 gs_reveal">
   <blockquote class= "title quote__text">I fell in love with making type punches and was very good at it, if I say so myself. </blockquote>
   <img src="" alt="">
   <img src="" alt="">
 </section>
 
 <section class= "part__2">
-  <div class= "chapter__2-container">
+  <div class= "chapter__2-container anim-3 gs_reveal">
     <h2 class= "chapter chapter__2">Chapter 2</h2>
     <h1 class= "title title__love">My first love</h1>
     <p class= "body__text-center body__text-padding">I fell in love with making <span class= "highlight">type punches</span>. In the years that followed, I took on countless of other jobs for companies that had to do with letter engraving. In 1720 <span class= "highlight">William Bowyer</span> asked me to design an English Arabic for Christian publications to be distributed in the Middle East.</p>
@@ -70,10 +70,10 @@
 </section>
 
 <section class= "part__2-1">
-  <h1 class= "title title__friends">With a little help from my friends</h1>
+  <h1 class= "title title__friends anim-3 gs_reveal">With a little help from my friends</h1>
   <div class ="sticky">
-    <p class= "body__text-center body__text-padding sticky">Although my success with the typefaces in England were almost instantaneous, I did not do this on my own. Banks gave me loans and sufficient money to go on. Having contact with printers, I was induced to <span class= "highlight">start up a type foundry</span>, largely through the encouragement of my dear friend William Bowyer.</p>
-    <p class= "body__text-center body__text-padding sticky">And off course, then there was my lovely wife Elizabeth who assisted me in getting the foundry reputation going. I met Elizabeth in the first years that I worked in London. </p>
+    <p class= "body__text-center body__text-padding sticky anim-3 gs_reveal">Although my success with the typefaces in England were almost instantaneous, I did not do this on my own. Banks gave me loans and sufficient money to go on. Having contact with printers, I was induced to <span class= "highlight">start up a type foundry</span>, largely through the encouragement of my dear friend William Bowyer.</p>
+    <p class= "body__text-center body__text-padding sticky anim-3 gs_reveal">And off course, then there was my lovely wife Elizabeth who assisted me in getting the foundry reputation going. I met Elizabeth in the first years that I worked in London. </p>
   </div>
 
   <div class= "shelf__container">
@@ -84,9 +84,9 @@
 
 <div class= "part__3">
   <section>
-    <h2 class= "chapter chapter__3">Chapter 3</h2>
-    <h1 class= "title title__success">Successful, but incomplete</h1>
-    <p class= "body__text success__text">Sure there where challenges, moments of doubt and setbacks. But from 1720 to 1780 few books were printed in England that did not use type from my foundry. Yes, I made a lot of typefaces in my years as a type founder. Still I felt a <span class= "highlight">burning desire</span> to keep moving. Why did I leave my simple life and home town? Well, I wanted to have a <span class= "highlight">lasting impact on the world</span>. And now I was ready.</p>
+    <h2 class= "chapter chapter__3 anim-1 gs_reveal">Chapter 3</h2>
+    <h1 class= "title title__success anim-1 gs_reveal">Successful, but incomplete</h1>
+    <p class= "body__text success__text anim-1 gs_reveal">Sure there where challenges, moments of doubt and setbacks. But from 1720 to 1780 few books were printed in England that did not use type from my foundry. Yes, I made a lot of typefaces in my years as a type founder. Still I felt a <span class= "highlight">burning desire</span> to keep moving. Why did I leave my simple life and home town? Well, I wanted to have a <span class= "highlight">lasting impact on the world</span>. And now I was ready.</p>
   </section>
 
   <section class= "quote-slide">
@@ -94,13 +94,13 @@
   </section>
 
   <section>
-    <div class= "specimen__text-container">
+    <div class= "specimen__text-container anim-3 gs_reveal">
       <p class= "specimen__text specimen__text-border body__text-padding">In <span class= "highlight">1722</span> my friend William Bowyer saw my desire, acknowledged my talent and asked me to cut <span class= "highlight">roman, italic, and Hebrew typefaces</span>. A new roman typeface with lots of character. It was first used in 1726 and a few years later it came to be called Caslon. Named after me of course.</p>
       <p class= "specimen__text body__text-padding">I issued my first official specimen sheet of the typeface in 1734 and exhibited the roman and italic types in 14 different sizes. The Caslon typeface eventually spread <span class= "highlight">all over Europe and the American colonies</span>. It became so popular that the expression “When in doubt, use Caslon” came about. I am really proud of that. </p>
     </div>
 
     <div>
-      <form action="">
+      <form class= "anim-3 gs_reveal" action="">
         <label for="input" class= "title__specimen">Make your own specimen sheet!</label><br>
         <input type="text" id="input" name="input" class= "input__field">
         <div>
@@ -133,24 +133,24 @@
 </div>
 
 <section class= "part__4">
-  <h2 class= "chapter chapter__4">Chapter 4</h2>
-  <h1 class= "title title__world">Changing the world</h1>
+  <h2 class= "chapter chapter__4 anim-1 gs_reveal">Chapter 4</h2>
+  <h1 class= "title title__world anim-1 gs_reveal">Changing the world</h1>
 
   <div>
-    <p class= "text__world body__text body__text-padding">Caslon is a <span class= "highlight">versatile typeface</span> that can be found in a wide variety of places. Places where history has been written. My typeface was considered the <span class= "highlight">font of choice for royalty and politics</span>.</p>
-    <p class= "chapter__4-middle body__text body__text-padding">I surpassed all of my competitors in no time and became the exclusive type foundry <span class= "highlight">for the King's printers</span>. Many great works of literature and documents of historical importance used my Caslon typeface.</p>
-    <p class= "text__world body__text body__text-padding">It was used in the first printed version of <span class= "highlight">the United States Declaration of Independence</span>. And George Bernard Shaw asked specifically that all his plays were set in the Caslon typeface. I never met George, but he seems like a nice guy.</p>
+    <p class= "text__world body__text body__text-padding anim-1 gs_reveal">Caslon is a <span class= "highlight">versatile typeface</span> that can be found in a wide variety of places. Places where history has been written. My typeface was considered the <span class= "highlight">font of choice for royalty and politics</span>.</p>
+    <p class= "chapter__4-middle body__text body__text-padding anim-1 gs_reveal">I surpassed all of my competitors in no time and became the exclusive type foundry <span class= "highlight">for the King's printers</span>. Many great works of literature and documents of historical importance used my Caslon typeface.</p>
+    <p class= "text__world body__text body__text-padding anim-1 gs_reveal">It was used in the first printed version of <span class= "highlight">the United States Declaration of Independence</span>. And George Bernard Shaw asked specifically that all his plays were set in the Caslon typeface. I never met George, but he seems like a nice guy.</p>
   </div>
 
   <div class= "img__shapes-container">
-    <img class= "img__shapes" src="<?php echo $img_shapes ?>" alt="shapes">
+    <img class= "img__shapes anim-2 gs_reveal" src="<?php echo $img_shapes ?>" alt="shapes">
   </div>
 </section>
 
 <section class= "part__5">
-  <h2 class= "chapter chapter__5">Chapter 5</h2>
-  <h1 class= "title title__characteristics">What makes my Caslon type so special, you might ask?</h1>
-  <p class= "characteristics__text">The Caslon typeface is extremely <span class= "highlight">legible, yet graceful and classic</span>. That is why a lot of printers like to use it for printed communication pieces. I designed the typeface with the <span class= "highlight">Old-style typeface</span> characteristics in mind. I like the look of the Old-style typefaces because of their greater refinement.</p>
+  <h2 class= "chapter chapter__5 anim-3 gs_reveal">Chapter 5</h2>
+  <h1 class= "title title__characteristics anim-3 gs_reveal">What makes my Caslon type so special, you might ask?</h1>
+  <p class= "characteristics__text anim-3 gs_reveal">The Caslon typeface is extremely <span class= "highlight">legible, yet graceful and classic</span>. That is why a lot of printers like to use it for printed communication pieces. I designed the typeface with the <span class= "highlight">Old-style typeface</span> characteristics in mind. I like the look of the Old-style typefaces because of their greater refinement.</p>
 </section>
 
 <section>
@@ -179,16 +179,16 @@
 
   <article class= "hidden-mobile">
 
-    <img class= "img__characteristics" src="<?php echo $img_characteristics ?>" alt="characteristics-img">
+    <img class= "img__characteristics anim-3 gs_reveal" src="<?php echo $img_characteristics ?>" alt="characteristics-img">
 
-    <div class= "characteristics__words">
+    <div class= "characteristics__words anim-3 gs_reveal">
       <p class= "characteristics__word" >wedged shape</p>
       <p class= "characteristics__word">horizontal crossbar</p>
       <p class= "characteristics__word">upright position</p>
       <p class= "characteristics__word">thin and thick strokes</p>
     </div>
 
-    <div class= "characteristic-container">
+    <div class= "characteristic-container anim-3 gs_reveal">
       <div class= "characteristic__text-container characteristic__text-container-1">
         <p class= "body__text-padding">There is a greater contrast between the thin and thick strokes, and that's why they are generally speaking sharper in appearance. </p>
         <p>Another major characteristic is the upright stress of the letterforms. Old-style typefaces have a more upright position.</p>
