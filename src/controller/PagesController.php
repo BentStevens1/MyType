9de628396a80778d7e749f34ccc32c2e359f962f
Bent <bent.stevens@student.howest.be>
2022-01-27 13:50:44 +0100

    code that gets images from database and gives it a variable name

--- src/controller/PagesController.php
+++ src/controller/PagesController.php
@@ -1,11 +1,16 @@
 <?php
 
 require_once __DIR__ . '/Controller.php';
+require_once __DIR__ .  '../../model/Image.php';
 
 
 class PagesController extends Controller {
 
   public function index() {
 
+    $images = Image::all();
+    foreach ($images as $image) {
+      $this->set($image->name, $image->value);
+    }
   }
 }
