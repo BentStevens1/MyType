2db70e5ac2880921c94ff35b448c6b9e5337dc93
Bent <bent.stevens@student.howest.be>
2022-01-27 12:50:38 +0100

    added javascript for tracking mouse and changing the standard cursor #6

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,17 @@
 export const init = () => {
   console.log('start executing this JavaScript');
- 
+
+  (function () {
+    const wrapper = document.querySelector('.page-wrapper');
+    const cursor = document.querySelector('.dot');
+
+    const editCursor = function editCursor(event) {
+      wrapper.style.left = event.clientX + 'px';
+      wrapper.style.top = event.clientY + 'px';
+      cursor.style.top = event.clientY + 'px';
+      cursor.style.left = event.clientX + 'px';
+    };
+    window.addEventListener('mousemove', editCursor);
+  })();
+
 };
