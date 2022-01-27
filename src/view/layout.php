b4416bbbd211b9d42eef12448c3a45a5b7725ba1
Bent <bent.stevens@student.howest.be>
2022-01-27 19:50:18 +0100

    changed the title name of the project

--- src/view/layout.php
+++ src/view/layout.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php echo $css; ?>
-  <title>Webpack - php - template</title>
+  <title>MyType Caslon</title>
   <link rel="stylesheet" href="https://use.typekit.net/apa8yeh.css">
 </head>
 <body>
