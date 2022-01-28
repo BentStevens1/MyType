a1876a4ad790e12673af9c4ec62e1de35d525d4f
Bent <bent.stevens@student.howest.be>
2022-01-28 13:39:39 +0100

    added code for the speciment sheet interaction #7

--- src/controller/PagesController.php
+++ src/controller/PagesController.php
@@ -12,5 +12,37 @@ class PagesController extends Controller {
     foreach ($images as $image) {
       $this->set($image->name, $image->value);
     }
+
+
+
+    if (!empty($_GET['input'])) {
+      $input_text = $_GET['input'];
+
+      if (strlen($input_text) >= 19) {
+        $result_lower = substr($input_text, 0, 19);
+        $result_upper = substr($repeat, 0, 12);
+
+      } else {
+        $remainder = 19 / strlen($input_text);
+        $number = ceil($remainder);
+
+        $repeat = str_repeat($input_text, $number);
+        $result_lower = substr($repeat, 0, 19);
+        $result_upper = substr($repeat, 0, 12);
+
+
+      }
+
+
+    } else {
+      $result_lower = "abcdefghjknoprstwxy";
+      $result_upper= "ABDEGHJMQRSW";
+
+
+    }
+
+    $this->set('result_lower', $result_lower);
+    $this->set('result_upper', $result_upper);
+
   }
 }
--- src/js/script.js
+++ src/js/script.js
@@ -8,20 +8,6 @@ export const init = () => {
   changeCursor();
   animations();
 
-  // document.addEventListener('DOMContentLoaded', function () {
-  //   gsap.registerPlugin(ScrollTrigger);
-  //   gsap.utils.toArray('.gs_reveal').forEach(function (elem) {
-  //     hide(elem); // assure that the element is hidden when scrolled into view
-  //     ScrollTrigger.create({
-  //       trigger: elem,
-  //       onEnter: function () { animateFrom(elem) },
-  //       // onEnterBack: function () { animateFrom(elem, -1) },
-  //       // onLeave: function () { hide(elem) } // assure that the element is hidden when scrolled into view
-  //     });
-  //   });
-  // });
-
-
 };
 
 
