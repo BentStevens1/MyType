a574c5f677f745333f2c0b10780f17b1d5b3c1a9
Bent <bent.stevens@student.howest.be>
2022-01-30 21:37:17 +0100

    corrected spelling mistakes

--- src/view/pages/index.php
+++ src/view/pages/index.php
@@ -16,11 +16,11 @@
 
 <section class= "introduction">
   <div class= "introduction__container">
-    <h1 class= "intro__title-mobile ">Hi, i'm William Caslon!</h1>
+    <h1 class= "intro__title-mobile ">Hi, I'm William Caslon!</h1>
     <img class= "img__william anim-2 gs_reveal" src="<?php echo $img_william ?>" alt="William-Caslon">
     <div class= "intro__text-container">
-      <h1 class= "intro__title-desktop anim-1 gs_reveal">Hi, i'm William Caslon!</h1>
-      <p class= "body__text intro__text anim-1 gs_reveal"> I am the <span class= "highlight">creator </span> of the legendary typeface Caslon. But I will tell you more about that later. Like every story my story starts at the beginning…</p>
+      <h1 class= "intro__title-desktop anim-1 gs_reveal">Hi, I'm William Caslon!</h1>
+      <p class= "body__text intro__text anim-1 gs_reveal"> I am the <span class= "highlight">creator </span> of the legendary typeface Caslon, I will tell you more about that later. Like every story mine also starts at the beginning…</p>
     </div>
   </div>
 </section>
@@ -31,7 +31,7 @@
   <div>
     <h2 class= "chapter chapter__1 ">Chapter 1</h2>
     <h1 class= "title title__life anim-1 gs_reveal">The simple life</h1>
-    <p class= "body__text anim-1 gs_reveal">It was a cold morning in the year 1692. I was born in a small village named <span class= "highlight">Cradley, Worcestershire</span>. I had a hard working family and from early on I was trained to be an engraver. Engraving is one of the oldest and most important techniques when producing images on paper in artistic printmaking. It was a simple life. Around the age of seventeen, I became an apprentice to a local <span class= "highlight">engraver of gunlocks and barrels</span>. That's where I started to learn all about the ins and out of metalwork.</p>
+    <p class= "body__text anim-1 gs_reveal">It was a cold morning in the year 1692. Born in a small village named <span class= "highlight">Cradley, Worcestershire</span>, I had a hard working family and from early on I was trained to become an engraver. Engraving is one of the oldest and most important techniques in artistic printmaking when producing images on paper. It was a simple life. Around the age of seventeen, I became an apprentice to a local <span class= "highlight">engraver of gunlocks and barrels</span>. That's where I started to learn all about the ins and out of metalwork.</p>
   </div>
 </section>
 
@@ -46,7 +46,7 @@
 
     <div class= "adventure__text">
       <p class= "body__text body__text-padding anim-2 gs_reveal">In <span class= "highlight">1716</span> I started a business in London. I was an official engraver of gun locks and barrels, and made tools for bookbinders. Metalwork is a form of art in my opinion, it requires precision and patience.</p>
-      <p class= "body__text body__text-padding anim-2 gs_reveal">For seven years I did numerous assignments for many different people. My metal craftsmanship was noticed by <span class= "highlight">the printer John Watts</span>. He gave me the task of cutting type punches for various presses in London. I challenged myself and took the job.</p>
+      <p class= "body__text body__text-padding anim-2 gs_reveal">For seven years I worked for many different people. My metal craftsmanship was noticed by <span class= "highlight">the printer John Watts</span>. He gave me the task of cutting type punches for various presses in London. I challenged myself and took the job.</p>
       <p class= "body__text body__text-padding anim-2 gs_reveal">It's from that moment that I became more and more interested in the <span class= "highlight">art of letter engraving</span>.</p>
     </div>
 
@@ -64,8 +64,8 @@
   <div class= "chapter__2-container anim-3 gs_reveal">
     <h2 class= "chapter chapter__2">Chapter 2</h2>
     <h1 class= "title title__love">My first love</h1>
-    <p class= "body__text-center body__text-padding">I began to make a lot of <span class= "highlight">type punches</span>. In the years that followed, I took on countless of other jobs for companies that had to do with letter engraving. In 1720 <span class= "highlight">William Bowyer</span> asked me to design an English Arabic for Christian publications to be distributed in the Middle East.</p>
-    <p class= "body__text-center body__text-padding">The typeface is now <span class= "highlight">used in psalter and the New Testament</span>. William saw the passion in me and we became good friends. Through jobs like this, the word spread about my type engraving.</p>
+    <p class= "body__text-center body__text-padding">I started to make a lot of <span class= "highlight">type punches</span>. In the following years, I took on countless jobs for companies that had to do with letter engraving. In 1720 <span class= "highlight">William Bowyer</span> asked me to design an English Arabic typeface for Christian publications to be distributed in the Middle East.</p>
+    <p class= "body__text-center body__text-padding">The typeface is now <span class= "highlight">used in psalter and the New Testament</span>. William saw my passion and we became good friends. Through jobs like this, the word spread about my type engraving.</p>
   </div>
 
   <div class= "type__punches">
@@ -80,8 +80,8 @@
 <section class= "part__2-1">
   <h1 class= "title title__friends anim-3 gs_reveal">With a little help from my friends</h1>
   <div class ="sticky anim-3 gs_reveal">
-    <p class= "body__text-center body__text-padding">Although my success with the typefaces in England were almost instantaneous, I did not do this on my own. Banks gave me loans and sufficient money to go on. Having contact with printers, I was induced to <span class= "highlight">start up a type foundry</span>, largely through the encouragement of my dear friend William Bowyer.</p>
-    <p class= "body__text-center body__text-padding">And off course, then there was my lovely wife Elizabeth who assisted me in getting the foundry reputation going. I met Elizabeth in the first years that I worked in London. </p>
+    <p class= "body__text-center body__text-padding">Although my success with typefaces in England was almost instantaneous, I did not do this on my own. Banks gave me loans and sufficient money to go on. Through various contacts with printers, I was encouraged to <span class= "highlight">start up a type foundry</span>, largely thanks to my dear friend William Bowyer.</p>
+    <p class= "body__text-center body__text-padding">And off course, there was my lovely wife Elizabeth who assisted me in getting the foundry reputation going. I met Elizabeth in the first years I worked in London. </p>
   </div>
 
   <div class= "shelf__container">
@@ -94,7 +94,7 @@
   <section>
     <h2 class= "chapter chapter__3 anim-1 gs_reveal">Chapter 3</h2>
     <h1 class= "title title__success anim-1 gs_reveal">Successful, but incomplete</h1>
-    <p class= "body__text success__text anim-1 gs_reveal">Sure there where challenges, moments of doubt and setbacks. But from 1720 to 1780 few books were printed in England that did not use type from my foundry. Yes, I made a lot of typefaces in my years as a type founder. Still I felt a <span class= "highlight">burning desire</span> to keep moving. Why did I leave my simple life and home town? Well, I wanted to have a <span class= "highlight">lasting impact on the world</span>. And now I was ready.</p>
+    <p class= "body__text success__text anim-1 gs_reveal">Sure there were challenges, moments of doubt and setbacks. But from 1720 to 1780 only few books were printed in England that did not use a type from my foundry. Yes, I made a lot of typefaces in my years as a type founder. Still I felt a <span class= "highlight">burning desire</span> to keep moving. Why did I leave my simple life and home town? Well, I wanted to have a <span class= "highlight">lasting impact on the world</span>. And now I was ready.</p>
   </section>
 
   <section class= "quote-slide">
@@ -103,8 +103,8 @@
 
   <section>
     <div class= "specimen__text-container anim-3 gs_reveal">
-      <p class= "specimen__text specimen__text-border body__text-padding">In <span class= "highlight">1722</span> my friend William Bowyer saw my desire, acknowledged my talent and asked me to cut <span class= "highlight">roman, italic, and Hebrew typefaces</span>. A new roman typeface with lots of character. It was first used in 1726 and a few years later it came to be called Caslon. Named after me of course.</p>
-      <p class= "specimen__text body__text-padding">I issued my first official specimen sheet of the typeface in 1734 and exhibited the roman and italic types in 14 different sizes. The Caslon typeface eventually spread <span class= "highlight">all over Europe and the American colonies</span>. It became so popular that the expression “When in doubt, use Caslon” came about. I am really proud of that. </p>
+      <p class= "specimen__text specimen__text-border body__text-padding">In <span class= "highlight">1722</span> my friend William Bowyer saw my desire, acknowledged my talent and asked me to cut <span class= "highlight">roman, italic, and Hebrew typefaces</span>. A new roman typeface with lots of character. It was first used in 1726 and a few years later it was named Caslon. Named after me of course.</p>
+      <p class= "specimen__text body__text-padding">I issued my first official specimen sheet of the typeface in 1734 and presented the roman and italic types in 14 different sizes. The Caslon typeface eventually spread <span class= "highlight">all over Europe and the American colonies</span>. It became so popular that the expression “When in doubt, use Caslon” was launched. I am really proud of that. </p>
     </div>
 
     <div id="form">
@@ -143,8 +143,8 @@
 <section class= "part__4">
   <h2 class= "chapter chapter__4 anim-1 gs_reveal">Chapter 4</h2>
   <h1 class= "title title__world anim-1 gs_reveal">Changing the world</h1>
-  <p class= "text__world body__text body__text-padding anim-1 gs_reveal">Caslon is a <span class= "highlight">versatile typeface</span> that can be found in a wide variety of places. Places where history has been written. My typeface was considered the <span class= "highlight">font of choice for royalty and politics</span>.</p>
-  <p class= "chapter__4-middle body__text body__text-padding anim-1 gs_reveal">I surpassed all of my competitors in no time and became the exclusive type foundry <span class= "highlight">for the King's printers</span>. Many great works of literature and documents of historical importance used my Caslon typeface.</p>
+  <p class= "text__world body__text body__text-padding anim-1 gs_reveal">Caslon is a <span class= "highlight">versatile typeface</span> that can be found in a wide variety of places. Places where history was written. My typeface was considered the <span class= "highlight">font of choice for royalty and politics</span>.</p>
+  <p class= "chapter__4-middle body__text body__text-padding anim-1 gs_reveal">I surpassed all of my competitors in no time and became the exclusive type foundry <span class= "highlight">of the King's printers</span>. Many great works of literature and documents of historical importance used my Caslon typeface.</p>
   <p class= "body__text arrow anim-1 gs_reveal"> Hover for magic!</p>
   <p class= "text__world body__text body__text-padding anim-1 gs_reveal declaration">It was used in the first printed version of <span class= "highlight">the United States Declaration of Independence</span>. And George Bernard Shaw asked specifically that all his plays were set in the Caslon typeface. I never met George, but he seems like a nice guy.</p>
 
@@ -161,14 +161,14 @@
 <section class= "part__5">
   <h2 class= "chapter chapter__5 anim-3 gs_reveal">Chapter 5</h2>
   <h1 class= "title title__characteristics anim-3 gs_reveal">What makes my Caslon type so special, you might ask?</h1>
-  <p class= "characteristics__text anim-3 gs_reveal">The Caslon typeface is extremely <span class= "highlight">legible, yet graceful and classic</span>. That is why a lot of printers like to use it for printed communication pieces. I designed the typeface with the <span class= "highlight">Old-style typeface</span> characteristics in mind. I like the look of the Old-style typefaces because of their greater refinement.</p>
+  <p class= "characteristics__text anim-3 gs_reveal">The Caslon typeface is extremely <span class= "highlight">legible, yet graceful and classic</span>. That is why a lot of printers like to use it for communication pieces. I designed the typeface with the <span class= "highlight">Old-style typeface</span> characteristics in mind. I like the look of the Old-style typefaces because of their greater refinement.</p>
 </section>
 
 <section class= "part__6">
   <article class= "hidden-dektop">
     <div>
       <img class= "circle anim-1 gs_reveal" src="<?php echo $circle_v ?>" alt="circle-v">
-      <p class= "body__text characteristic__text anim-2 gs_reveal">There is a greater contrast between the <span class= "highlight">thin and thick strokes</span>, and that's why they are generally speaking sharper in appearance. </p>
+      <p class= "body__text characteristic__text anim-2 gs_reveal">There is a greater contrast between the <span class= "highlight">thin and thick strokes</span>, that's why they are generally speaking sharper in appearance. </p>
     </div>
 
     <div>
@@ -183,7 +183,7 @@
 
     <div>
       <img class= "circle anim-1 gs_reveal" src="<?php echo $circle_e ?>" alt="circle-e">
-      <p class= "body__text characteristic__text anim-2 gs_reveal">The <span class= "highlight">horizontal crossbars</span>, that you can see by the “e” for example, are also typical for Old-style typefaces. This is something that is not the case with the Venetian typefaces. That is why i don't like them so much. I hate them frankly.</p>
+      <p class= "body__text characteristic__text anim-2 gs_reveal">The <span class= "highlight">horizontal crossbars</span>, as shown in the “e” for example, are also typical for Old-style typefaces. This is something that is not the case with the Venetian typefaces. That is why I don't like them so much. I hate them frankly.</p>
     </div>
 
   </article>
@@ -201,13 +201,13 @@
 
     <div class= "characteristic-container anim-3 gs_reveal">
       <div class= "characteristic__text-container characteristic__text-container-1">
-        <p class= "body__text-padding">There is a greater contrast between the <span class= "highlight">thin and thick strokes</span>, and that's why they are generally speaking sharper in appearance. </p>
+        <p class= "body__text-padding">There is a greater contrast between the <span class= "highlight">thin and thick strokes</span>, that's why they are generally speaking sharper in appearance. </p>
         <p>Another major characteristic is the upright stress of the letterforms. Old-style typefaces have a more <span class= "highlight">upright position</span>.</p>
       </div>
 
       <div class= "characteristic__text-container">
         <p class= "body__text-padding">Perhaps the most notable characteristic is that the ascenders have a <span class= "highlight">wedged shape</span>. </p>
-        <p>The <span class= "highlight">horizontal crossbars</span>, that you can see by the “e” for example, are also typical for Old-style typefaces. This is something that is not the case with the Venetian typefaces. That is why i don't like them so much. I hate them frankly.</p>
+        <p>The <span class= "highlight">horizontal crossbars</span>, as shown in the “e” for example, are also typical for Old-style typefaces. This is something that is not the case with the Venetian typefaces. That is why I don't like them so much. I hate them frankly.</p>
       </div>
     </div>
   </article>
@@ -220,7 +220,7 @@
   <article class= "slanted__outro anim-1 gs_reveal">
     <h1 class= "slanted__title">This was the story of Caslon</h1>
     <h2 class= "slanted__subtitle">Liked this experience?</h2>
-    <p class= "slanted__text">This is a series where we bring some legendary fonts into focus. Every week we release a new immersive experience that brings you the rich typographical history and the fascinating designers of a certain typeface. <a class="highlighted__red" href="https://www.slanted.de/"> Slanted<a> inspires. </p>
+    <p class= "slanted__text">In these series we focus on legendary fonts. Every week we release a new immersive experience that tells the rich typographical history of a certain typeface and its fascinating designer. <a class="highlighted__red" href="https://www.slanted.de/"> Slanted<a> inspires. </p>
     <a class= "slanted__button" href="https://www.slanted.de/shop">discover our magazines</a>
   </article>
 
